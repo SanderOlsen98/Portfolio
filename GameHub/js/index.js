a5edcb9cad7723ccf2f52261933a8973b1c0f41e
SanderOlsen98 <sanderlolsen@gmail.com>
2021-12-13 18:06:40 +0100

    update

--- GameHub/js/index.js
+++ GameHub/js/index.js
@@ -1,12 +1,12 @@
-const baseUrl = "https://sanderolsentestsites.one/wp-json/wc/store/products/";
+const baseUrl = "http://tonjeebest.no/GameHub/wp-json/wc/store/products/";
 const productContainer = document.querySelector("#customID");
 
 async function getProducts(baseUrl) {
-    const response = await fetch (baseUrl);
-    const products = await response.json();
-    
-    products.forEach(function(product){
-        productContainer.innerHTML += `
+  const response = await fetch(baseUrl);
+  const products = await response.json();
+
+  products.forEach(function (product) {
+    productContainer.innerHTML += `
         <div class="products">
         <a href="details.html?game=${product.name}">
         <img src="${product.images[0].src}">
@@ -14,8 +14,7 @@ async function getProducts(baseUrl) {
         <h3>${product.name}</h3>
         <a class="add-cart price" onclick="alertbox()">${product.prices.price}$</a>
         </div>`;
-    })
-
+  });
 }
 
 getProducts(baseUrl);
