d38af8b342b0b5264ab91ab2017202db2bd50b43
SanderOlsen98 <sanderlolsen@gmail.com>
2021-12-15 17:01:00 +0100

    update

--- BlogSite/js/images.js
+++
@@ -1,9 +0,0 @@
-const images = document.querySelectorAll(".image-carousel img");
-
-images.forEach(function(image){
-    image.onclick = function(event) {
-        document.querySelector(".selected-image").classList.remove("selected-image");
-        const clickParent = event.target.parentNode;
-        clickParent.classList.add("selected-image");
-    }
-})
--- CommunityScienceMuseum/README.md
+++
@@ -1,4 +0,0 @@
-# Community-Science-Museum
-Explore Together
-Semester Project 1 - HTML, CSS, Adobe XD
-A Website Created By Sander Lindberg Olsen
